580bbc7496daf8574adc322dfe44d280a5790d7f
Matias Lopez <matias.lopez.renato@gmail.com>
2024-07-18 02:43:08 -0400

    Configuración correcta para despliegue en GitHub Page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/My-portfolio/',
   plugins: [react()],
+  base: 'https://matic539.github.io/My-portfolio',
 })
