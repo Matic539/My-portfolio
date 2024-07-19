889a81f3b073b89c552155d2b1c51debbe8229fe
Matias Lopez <matias.lopez.renato@gmail.com>
2024-07-18 23:26:32 -0400

    Resueltos conflictos después del pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './router';
 import './assets/styles/App.css';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
 import './assets/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
+  // React.StrictMode
 )
